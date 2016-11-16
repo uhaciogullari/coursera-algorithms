fb7daa79c5151a298e2286be796becd6d38bd98f
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2016-11-16 22:17:08 +0100

    Added tests for QuickFind and QuickUnion

---
+++ src/Algorithms/UnionFindExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithms
+{
+    public static class UnionFindExtensions
+    {
+        public static bool TryAdd(this IUnionFind unionFind, int p, int q)
+        {
+            if (unionFind.IsConnected(p, q))
+            {
+                return false;
+            }
+
+            unionFind.Union(p,q);
+            return true;
+        }
+    }
+}
---
+++ src/Algorithms/UnionFindTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms
+{
+    public abstract class UnionFindTests
+    {
+        protected abstract IUnionFind CreateUnionFindAlgorithm(int size);
+
+        [Fact]
+        public void UnionFindTest()
+        {
+            var algorithm = CreateUnionFindAlgorithm(10);
+
+            algorithm.TryAdd(4, 3).Should().BeTrue();
+            algorithm.TryAdd(3, 8).Should().BeTrue();
+            algorithm.TryAdd(6, 5).Should().BeTrue();
+            algorithm.TryAdd(9, 4).Should().BeTrue();
+            algorithm.TryAdd(2, 1).Should().BeTrue();
+            algorithm.TryAdd(8, 9).Should().BeFalse();
+            algorithm.TryAdd(5, 0).Should().BeTrue();
+            algorithm.TryAdd(7, 2).Should().BeTrue();
+            algorithm.TryAdd(6, 1).Should().BeTrue();
+            algorithm.TryAdd(1, 0).Should().BeFalse();
+            algorithm.TryAdd(6, 7).Should().BeFalse();
+        }
+    }
+
+    public class QuickFindTests : UnionFindTests
+    {
+        protected override IUnionFind CreateUnionFindAlgorithm(int size)
+        {
+            return new QuickFind(size);
+        }
+    }
+
+    public class QuickUnionTests: UnionFindTests
+    {
+        protected override IUnionFind CreateUnionFindAlgorithm(int size)
+        {
+            return new QuickUnion(size);
+        }
+    }
+}
